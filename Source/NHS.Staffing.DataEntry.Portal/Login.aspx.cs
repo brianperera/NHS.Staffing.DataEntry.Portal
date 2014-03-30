fc5d51fdacc146e13e13c4f607b5f6eb7d6950ae
brianperera <briano_p@yahoo.com>
2014-03-30 09:26:09 +0530

    failure text added

--- Source/NHS.Staffing.DataEntry.Portal/Login.aspx.cs
+++ Source/NHS.Staffing.DataEntry.Portal/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 
 namespace Nhs.Staffing.DataEntry.Portal
 {
@@ -14,6 +15,17 @@ namespace Nhs.Staffing.DataEntry.Portal
             Master.FindControl("NavigationBar").Visible = false;
             Master.FindControl("HosName").Visible = false;
             Master.FindControl("NhsName").Visible = false;
+        }
+
+        protected void LoginUser_LoginError(object sender, EventArgs e)
+        {
+            // Add the failure text to the validation group
+            CustomValidator failureTextValidator = new CustomValidator();
+            failureTextValidator.IsValid = false;
+            failureTextValidator.ErrorMessage = LoginUser.FailureText;
+            failureTextValidator.ValidationGroup = "LoginUserValidationGroup";
+            failureTextValidator.CssClass = "alert-danger";
+            this.Page.Validators.Add(failureTextValidator);
         }
     }
 }
